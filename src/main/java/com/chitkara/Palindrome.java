4eca4399a17358ecf64796038fd4d1e6da3ee4a4
shivamrishi98 <shivamrishisr@gmail.com>
2020-06-26 13:05:07 +0530

    project done

---
+++ src/main/java/com/chitkara/Palindrome.java
@@ -0,0 +1,24 @@
+package com.chitkara;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Palindrome {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        palindromes(Stream.generate(scanner::nextLine).
+                limit(10).
+                collect(Collectors.toList())).
+                forEach(System.out::println);
+    }
+
+    private static List<String> palindromes(List<String> list) {
+        return list.
+                stream().
+                filter(str -> new StringBuilder(str).reverse().toString().equals(str)).
+                collect(Collectors.toList());
+    }
+}
